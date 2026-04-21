86faffc7f050f4c8ba4323f6604f962a86a86bad
“John <john@email.com>
2026-04-21 19:30:06 +0300

    fix

---
+++ main.py
@@ -0,0 +1,11 @@
+name = "Alex"
+surname = 'Bush'
+nickname = """-"'Big brother'"-"""
+quote = "He said: 'Дорогу я не знаю.'"
+
+print(surname)
+print(nickname)
+print(quote)
+
+my_hobbies = "теніс футбол і смачненько поїсти"
+print(my_hobbies)
