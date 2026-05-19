7ccc3a479d1c51aa33aa62ad730f042970efd9f9
“John <john@email.com>
2026-05-19 20:55:57 +0300

    fix

--- comments.py
+++ comments.py
@@ -5,3 +5,5 @@ response = requests.get(url=url, params=params)
 response_json = response.json()
 comments = response_json["comments"]
 print(comments)
+for comment in comments :
+    print (comments) 
