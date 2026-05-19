3cae3d3ad75be435d51652a8b9619f79b668d8ea
“John <john@email.com>
2026-05-19 20:40:18 +0300

    fix

--- comments.py
+++ comments.py
@@ -1,6 +1,7 @@
 import requests
 url = "https://dummyjson.com/comments"
 params = {"limit" : 0}
-response = requests.get(url, params=params)
-
-
+response = requests.get(url=url, params=params)
+response_json = response.json()
+comments = response_json["comments"]
+print(comments)
