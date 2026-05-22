f5cc9abc1a2f8a14a43298f3983ccd306ef7c418
“John <john@email.com>
2026-05-22 20:11:27 +0300

    fix

---
+++ images.py
@@ -0,0 +1,4 @@
+PIZZA_IMAGE = 'https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg'
+BURGER_IMAGE = 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/RedDot_Burger.jpg/1280px-RedDot_Burger.jpg'
+CAUSE_IMAGE = 'https://embed.widencdn.net/img/beef/t7czsycsiw/360x360px/AdobeStock_148561805.jpeg'
+COLA_IMAGE = 'https://content2.rozetka.com.ua/goods/images/big/622926010.png'
